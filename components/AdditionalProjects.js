307bd1f14a4296dd38dd352913758482be87a6d1
thescripted <somethingscripted@gmail.com>
2020-07-08 14:58:44 -0500

    Styled Card Component

--- components/AdditionalProjects.js
+++ components/AdditionalProjects.js
@@ -1,11 +1,21 @@
 import { CardItem } from "@components/CardItem"
 export const AdditionalProjects = () => {
   return (
-    <ul className="additional">
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-    </ul>
+    <>
+      <div className="additional">
+        <CardItem />
+      </div>
+      <style jsx>{`
+        .additional {
+          display: flex;
+          flex-direction: column;
+        }
+        .additional-duplet {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+      `}</style>
+    </>
   )
 }
--- components/CardItem.js
+++ components/CardItem.js
@@ -1,7 +1,49 @@
+import { getDisplayName } from "next/dist/next-server/lib/utils"
+
 export const CardItem = props => {
   return (
-    <li className="card-item">
-      <p>Card</p>
-    </li>
+    <>
+      <div className="card-item">
+        <img src="example1.jpg" alt="project image" />
+        <div className="card-content">
+          <h3>TypeScript TodoList</h3>
+          <p style={{ fontSize: "12px" }}>To Do List built with TypeScript</p>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .card-item {
+            display: flex;
+            flex-direction: row;
+            width: 300px;
+            height: 100px;
+            border-radius: 5px;
+            background: hsla(150, 90%, 66%, 0.4);
+            overflow: hidden;
+            box-shadow: 0 1px 1px 0 rgba(66, 66, 66, 0.08),
+              0 1px 3px 1px rgba(66, 66, 66, 0.16);
+            transition: 0.1s;
+             {
+            }
+          }
+
+          .card-item:hover {
+            background: hsla(150, 90%, 56%, 0.6);
+          }
+
+          .card-item img {
+            width: 25%;
+            max-height: 100%;
+          }
+
+          .card-content {
+            padding: 16px 20px 12px;
+          }
+          .card-content h3 {
+            margin-bottom: 8px;
+          }
+        `}
+      </style>
+    </>
   )
 }
